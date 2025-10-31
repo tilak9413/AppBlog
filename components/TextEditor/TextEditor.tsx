3af49ea31ca64c48aa01ce7d41b7091d2a5df512
tilak kumar <135198932+tilak9413@users.noreply.github.com>
2025-11-01 00:45:41 +0530

    asdasd

--- components/TextEditor/TextEditor.tsx
+++ components/TextEditor/TextEditor.tsx
@@ -3,48 +3,48 @@ import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 interface WpCKEditorProps {
-    initialContent?: string;
-    onContentChange?: (content: string) => void;
-    label: string;
-    required?: boolean;
-    editor?: any;
+  initialContent?: string;
+  onContentChange?: (content: string) => void;
+  label: string;
+  required?: boolean;
+  editor?: any;
 }
 
 const TextEditor: React.FC<WpCKEditorProps> = ({
-    initialContent = "",
-    onContentChange,
-    label,
-    required,
-    editor = ClassicEditor,
+  initialContent = "",
+  onContentChange,
+  label,
+  required,
+  editor = ClassicEditor,
 }) => {
-    const [content, setContent] = useState<string>(initialContent);
+  const [content, setContent] = useState<string>(initialContent);
 
-    const handleChange = (_event: any, editorInstance: any) => {
-        const data = editorInstance.getData();
-        setContent(data);
-        onContentChange?.(data);
-    };
+  const handleChange = (_event: any, editorInstance: any) => {
+    const data = editorInstance.getData();
+    setContent(data);
+    onContentChange?.(data);
+  };
 
-    return (
-        <>
-            {label && (
-                <label
-                    style={{ fontSize: "12px" }}
-                    className={`fw-medium ms-1 ${required ? "required" : ""}`}
-                >
-                    {label}
-                </label>
-            )}
+  return (
+    <>
+      {label && (
+        <label
+          style={{ fontSize: "12px" }}
+          className={`fw-medium ms-1 ${required ? "required" : ""}`}
+        >
+          {label}
+        </label>
+      )}
 
-            <div className="wp-ckeditor-container">
-                <CKEditor
-                    editor={editor}
-                    data={content}
-                    onChange={handleChange}
-                />
-            </div>
-        </>
-    );
+      <div className="wp-ckeditor-container">
+        <CKEditor
+          editor={editor as any}
+          data={content}
+          onChange={handleChange}
+        />
+      </div>
+    </>
+  );
 };
 
 export default TextEditor;
